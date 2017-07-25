93d690bf1322e4667f25844cbb940a6e8a074a0b
DiegoPergolini <diego.pergolini@studio.unibo.it>
2017-07-25 16:16:07 +0200

    Various task

--- app/src/main/java/com/example/luca/planit/SignupActivity.java
+++ app/src/main/java/com/example/luca/planit/SignupActivity.java
@@ -6,11 +6,15 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -324,104 +328,125 @@ public class SignupActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Result result) {
-            if (result.getResult().equals(RequestResult.OK_REGISTRATION)) {
+            if (result.getResult() == RequestResult.NO_CONNECTION) {
+                new AlertDialog.Builder(this.context)
+                        .setMessage("Check your connection")
+                        .setTitle("Internet not available")
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .show();
+            }else {
+                if (result.getResult().equals(RequestResult.OK_REGISTRATION)) {
 
-                Account account = result.getAccount();
-                String name = account.getName();
-                String surname = account.getSurname();
-                String email = account.getEmail();
-                String username = account.getUsername();
-                String password = account.getPassword();
-                String bornDate = account.getBorndate();
-                String id = account.getId();
+                    Account account = result.getAccount();
+                    String name = account.getName();
+                    String surname = account.getSurname();
+                    String email = account.getEmail();
+                    String username = account.getUsername();
+                    String password = account.getPassword();
+                    String bornDate = account.getBorndate();
+                    String id = account.getId();
 
-                SharedPreferences prefs = getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE);
-                SharedPreferences.Editor editor = prefs.edit();
+                    SharedPreferences prefs = getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE);
+                    SharedPreferences.Editor editor = prefs.edit();
 
-                editor.putString(getString(R.string.name_pref), name);
-                editor.putString(getString(R.string.surname_pref), surname);
-                editor.putString(getString(R.string.email_pref), email);
-                editor.putString(getString(R.string.username_pref), username);
-                editor.putString(getString(R.string.password_pref), password);
-                editor.putString(getString(R.string.born_date_pref), bornDate);
-                editor.putString(getString(R.string.id_pref), id);
+                    editor.putString(getString(R.string.name_pref), name);
+                    editor.putString(getString(R.string.surname_pref), surname);
+                    editor.putString(getString(R.string.email_pref), email);
+                    editor.putString(getString(R.string.username_pref), username);
+                    editor.putString(getString(R.string.password_pref), password);
+                    editor.putString(getString(R.string.born_date_pref), bornDate);
+                    editor.putString(getString(R.string.id_pref), id);
 
-                editor.apply();
+                    editor.apply();
 
-                Intent intent = new Intent(getApplication(), HomeActivity.class);
-                intent.putExtra(getString(R.string.extra_from_signup), true);
-                startActivity(intent);
-                ((Activity) context).finish();
-            } else {
-                showProgress(false);
-                if (result.getResult().equals(RequestResult.MAIL_ALREADY_PRESENT)) {
-                    emailEditText.setError(getString(R.string.error_already_present_email));
-                    emailEditText.requestFocus();
-                } else if (result.getResult().equals(RequestResult.USERNAME_ALREADY_EXISTING)) {
-                    usernameEditText.setError(getString(R.string.error_already_existing_username));
-                    usernameEditText.requestFocus();
+                    Intent intent = new Intent(getApplication(), HomeActivity.class);
+                    intent.putExtra(getString(R.string.extra_from_signup), true);
+                    startActivity(intent);
+                    ((Activity) context).finish();
+                } else {
+                    showProgress(false);
+                    if (result.getResult().equals(RequestResult.MAIL_ALREADY_PRESENT)) {
+                        emailEditText.setError(getString(R.string.error_already_present_email));
+                        emailEditText.requestFocus();
+                    } else if (result.getResult().equals(RequestResult.USERNAME_ALREADY_EXISTING)) {
+                        usernameEditText.setError(getString(R.string.error_already_existing_username));
+                        usernameEditText.requestFocus();
+                    }
                 }
             }
+
         }
 
         @Override
         protected Result doInBackground(RegistrationData... params) {
             try {
-                URL url = new URL(Resource.BASE_URL + Resource.REGISTRATION_PAGE); //Enter URL here
-                JSONObject returned = null;
-                httpURLConnection = (HttpURLConnection) url.openConnection();
-                httpURLConnection.setUseCaches(false);
-                httpURLConnection.setDoOutput(true);
-                httpURLConnection.setDoInput(true);
-                httpURLConnection.setRequestMethod("POST"); // here you are telling that it is a POST request, which can be changed into "PUT", "GET", "DELETE" etc.
-                //httpURLConnection.setRequestProperty("Content-Type", "application/json"); // here you are setting the `Content-Type` for the data you are sending which is `application/json`
-                OutputStream os = httpURLConnection.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
-                HashMap<String, String> toPass = new HashMap<>();
-                toPass.put("email", params[0].getEmail());
-                toPass.put("password", params[0].getPassword());
-                toPass.put("nome", params[0].getName());
-                toPass.put("cognome", params[0].getSurname());
-                toPass.put("username", params[0].getUsername());
-                toPass.put("data_nascita", params[0].getBorndate());
-                writer.write(getPostDataString(toPass));
-                writer.flush();
-                writer.close();
-                os.close();
-                httpURLConnection.connect();
-                int responseCode = httpURLConnection.getResponseCode();
-                if (responseCode == httpURLConnection.HTTP_OK) {
-                    InputStream inputStream = httpURLConnection.getInputStream();
-                    rd = new BufferedReader(new InputStreamReader(inputStream));
-                    String line = "";
-                    while ((line = rd.readLine()) != null) {
-                        response.append(line);
+                ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+                if (networkInfo != null && networkInfo.isConnected()) {
+                    URL url = new URL(Resource.BASE_URL + Resource.REGISTRATION_PAGE); //Enter URL here
+                    JSONObject returned = null;
+                    httpURLConnection = (HttpURLConnection) url.openConnection();
+                    httpURLConnection.setUseCaches(false);
+                    httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setDoInput(true);
+                    httpURLConnection.setRequestMethod("POST"); // here you are telling that it is a POST request, which can be changed into "PUT", "GET", "DELETE" etc.
+                    //httpURLConnection.setRequestProperty("Content-Type", "application/json"); // here you are setting the `Content-Type` for the data you are sending which is `application/json`
+                    OutputStream os = httpURLConnection.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                    HashMap<String, String> toPass = new HashMap<>();
+                    toPass.put("email", params[0].getEmail());
+                    toPass.put("password", params[0].getPassword());
+                    toPass.put("nome", params[0].getName());
+                    toPass.put("cognome", params[0].getSurname());
+                    toPass.put("username", params[0].getUsername());
+                    toPass.put("data_nascita", params[0].getBorndate());
+                    writer.write(getPostDataString(toPass));
+                    writer.flush();
+                    writer.close();
+                    os.close();
+                    httpURLConnection.connect();
+                    int responseCode = httpURLConnection.getResponseCode();
+                    if (responseCode == httpURLConnection.HTTP_OK) {
+                        InputStream inputStream = httpURLConnection.getInputStream();
+                        rd = new BufferedReader(new InputStreamReader(inputStream));
+                        String line = "";
+                        while ((line = rd.readLine()) != null) {
+                            response.append(line);
+                        }
+                        returned = new JSONObject(response.toString());
                     }
-                    returned = new JSONObject(response.toString());
-                }
-                if (returned.getString("result").equals(RequestResult.USERNAME_ALREADY_EXISTING.toString())) {
-                    toReturn = new Result(RequestResult.USERNAME_ALREADY_EXISTING);
-                    Log.d("risposta", "Credenziali errate");
-                } else if (returned.getString("result").equals(RequestResult.MAIL_ALREADY_PRESENT.toString())) {
-                    toReturn = new Result(RequestResult.MAIL_ALREADY_PRESENT);
-                    Log.d("risposta", "Credenziali errate");
-                } else {
-                    try {
-                        Account loggedAccount = new AccountImpl.Builder()
-                                .setBorndate(params[0].getBorndate())
-                                .setEmail(params[0].getEmail())
-                                .setId(returned.getString("result"))
-                                .setName(params[0].getName())
-                                .setSurname(params[0].getSurname())
-                                .setPassword(params[0].getPassword())
-                                .setUsername(params[0].getUsername())
-                                .build();
-                        toReturn = new Result(RequestResult.OK_REGISTRATION, loggedAccount);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+                    if (returned.getString("result").equals(RequestResult.USERNAME_ALREADY_EXISTING.toString())) {
+                        toReturn = new Result(RequestResult.USERNAME_ALREADY_EXISTING);
+                        Log.d("risposta", "Credenziali errate");
+                    } else if (returned.getString("result").equals(RequestResult.MAIL_ALREADY_PRESENT.toString())) {
+                        toReturn = new Result(RequestResult.MAIL_ALREADY_PRESENT);
+                        Log.d("risposta", "Credenziali errate");
+                    } else {
+                        try {
+                            Account loggedAccount = new AccountImpl.Builder()
+                                    .setBorndate(params[0].getBorndate())
+                                    .setEmail(params[0].getEmail())
+                                    .setId(returned.getString("result"))
+                                    .setName(params[0].getName())
+                                    .setSurname(params[0].getSurname())
+                                    .setPassword(params[0].getPassword())
+                                    .setUsername(params[0].getUsername())
+                                    .build();
+                            toReturn = new Result(RequestResult.OK_REGISTRATION, loggedAccount);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
+                    }
+                }else {
+                    return new Result(RequestResult.NO_CONNECTION);
                 }
+
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
