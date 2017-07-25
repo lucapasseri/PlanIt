f9cddba23042bfc9ecd95ab3ecd36e423322fce1
DiegoPergolini <diego.pergolini@studio.unibo.it>
2017-07-25 15:54:17 +0200

    Various task

--- app/src/main/java/com/example/luca/planit/LoginActivity.java
+++ app/src/main/java/com/example/luca/planit/LoginActivity.java
@@ -5,14 +5,18 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v4.content.ContextCompat;
 
 import android.os.AsyncTask;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
@@ -141,7 +145,7 @@ public class LoginActivity extends Activity {
             showProgress(true);
             LoginTask loginTask = new LoginTask(this);
 
-            if(emailUser.contains("@")) {
+            if (emailUser.contains("@")) {
                 loginTask.execute(LoginData.getLoginDataInstanceByEmail(emailUser, password));
             } else {
                 Log.d("tag1", "ciao");
@@ -159,7 +163,7 @@ public class LoginActivity extends Activity {
 
         View view = this.getCurrentFocus();
         if (view != null) {
-            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
 
@@ -189,7 +193,7 @@ public class LoginActivity extends Activity {
         }
     }
 
-    private class LoginTask extends AsyncTask<LoginData,Void,Result> {
+    private class LoginTask extends AsyncTask<LoginData, Void, Result> {
 
         private final Context context;
 
@@ -197,81 +201,94 @@ public class LoginActivity extends Activity {
             this.context = context;
         }
 
-        private String getPostDataString(HashMap<String,String > params) throws UnsupportedEncodingException {
+        private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
             StringBuilder result = new StringBuilder();
             boolean first = true;
-            for(Map.Entry<String, String> entry : params.entrySet()){
-                if(first)
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+                if (first)
                     first = false;
                 else
                     result.append("&");
-                result.append(URLEncoder.encode(entry.getKey(),"UTF-8"));
+                result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
                 result.append("=");
-                result.append(URLEncoder.encode(entry.getValue(),"UTF-8"));
+                result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
             }
 
-            return  result.toString();
+            return result.toString();
         }
 
         //Nome dei parametri del json di risposta
 
 
-        Result toReturn ;
+        Result toReturn;
         HttpURLConnection httpURLConnection = null;
         StringBuilder response = new StringBuilder();
         BufferedReader rd = null;
 
         @Override
         protected void onPostExecute(Result result) {
-            if(result.getResult()== (RequestResult.OK_LOGIN)){
-
-                Account account = result.getAccount();
-                String name = account.getName();
-                String surname = account.getSurname();
-                String email = account.getEmail();
-                String username = account.getUsername();
-                String password = account.getPassword();
-                String bornDate = account.getBorndate();
-                String id = account.getId();
-
-                SharedPreferences prefs = getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE);
-                SharedPreferences.Editor editor = prefs.edit();
-                Set<String> emailSet = new HashSet<>();
-                Set<String> emailPrefs = prefs.getStringSet(getString(R.string.email_user_list_pref), new HashSet<String>());
-
-                for (String e : emailPrefs) {
-                    emailSet.add(e);
-                }
-
-                emailSet.add(email);
-                emailSet.add(username);
-
-                editor.putStringSet(getString(R.string.email_user_list_pref), emailSet);
-                editor.putString(getString(R.string.name_pref), name);
-                editor.putString(getString(R.string.surname_pref), surname);
-                editor.putString(getString(R.string.email_pref), email);
-                editor.putString(getString(R.string.username_pref), username);
-                editor.putString(getString(R.string.password_pref), password);
-                editor.putString(getString(R.string.born_date_pref), bornDate);
-                editor.putString(getString(R.string.id_pref), id);
-
-                editor.apply();
-
-                Intent intent = new Intent(getApplication(), HomeActivity.class);
-                intent.putExtra(getString(R.string.extra_from_login), true);
-                startActivity(intent);
-                ((Activity) context).finish();
+            if (result.getResult() == RequestResult.NO_CONNECTION) {
+                new AlertDialog.Builder(this.context)
+                        .setMessage("Check your connection")
+                        .setTitle("Internet not available")
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .show();
             } else {
-                showProgress(false);
-                if(result.getResult() == RequestResult.MAIL_NOT_PRESENT) {
-                    emailEditText.setError(getString(R.string.error_invalid_email));
-                    emailEditText.requestFocus();
-                } else if(result.getResult() == RequestResult.WRONG_PASSWORD) {
-                    passwordEditText.setError(getString(R.string.error_incorrect_password));
-                    passwordEditText.requestFocus();
-                } else if(result.getResult() == RequestResult.USERNAME_NOT_PRESENT) {
-                    emailEditText.setError(getString(R.string.error_invalid_email));
-                    emailEditText.requestFocus();
+                if (result.getResult() == (RequestResult.OK_LOGIN)) {
+
+                    Account account = result.getAccount();
+                    String name = account.getName();
+                    String surname = account.getSurname();
+                    String email = account.getEmail();
+                    String username = account.getUsername();
+                    String password = account.getPassword();
+                    String bornDate = account.getBorndate();
+                    String id = account.getId();
+
+                    SharedPreferences prefs = getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE);
+                    SharedPreferences.Editor editor = prefs.edit();
+                    Set<String> emailSet = new HashSet<>();
+                    Set<String> emailPrefs = prefs.getStringSet(getString(R.string.email_user_list_pref), new HashSet<String>());
+
+                    for (String e : emailPrefs) {
+                        emailSet.add(e);
+                    }
+
+                    emailSet.add(email);
+                    emailSet.add(username);
+
+                    editor.putStringSet(getString(R.string.email_user_list_pref), emailSet);
+                    editor.putString(getString(R.string.name_pref), name);
+                    editor.putString(getString(R.string.surname_pref), surname);
+                    editor.putString(getString(R.string.email_pref), email);
+                    editor.putString(getString(R.string.username_pref), username);
+                    editor.putString(getString(R.string.password_pref), password);
+                    editor.putString(getString(R.string.born_date_pref), bornDate);
+                    editor.putString(getString(R.string.id_pref), id);
+
+                    editor.apply();
+
+                    Intent intent = new Intent(getApplication(), HomeActivity.class);
+                    intent.putExtra(getString(R.string.extra_from_login), true);
+                    startActivity(intent);
+                    ((Activity) context).finish();
+                } else {
+                    showProgress(false);
+                    if (result.getResult() == RequestResult.MAIL_NOT_PRESENT) {
+                        emailEditText.setError(getString(R.string.error_invalid_email));
+                        emailEditText.requestFocus();
+                    } else if (result.getResult() == RequestResult.WRONG_PASSWORD) {
+                        passwordEditText.setError(getString(R.string.error_incorrect_password));
+                        passwordEditText.requestFocus();
+                    } else if (result.getResult() == RequestResult.USERNAME_NOT_PRESENT) {
+                        emailEditText.setError(getString(R.string.error_invalid_email));
+                        emailEditText.requestFocus();
+                    }
                 }
             }
         }
@@ -279,80 +296,91 @@ public class LoginActivity extends Activity {
         @Override
         protected Result doInBackground(LoginData... params) {
             try {
-                URL url = new URL(Resource.BASE_URL+Resource.LOGIN_PAGE); //Enter URL here
+                ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+                if (networkInfo != null && networkInfo.isConnected()) {
+                    URL url = new URL(Resource.BASE_URL + Resource.LOGIN_PAGE); //Enter URL here
 
-                httpURLConnection = (HttpURLConnection)url.openConnection();
 
-                httpURLConnection.setUseCaches(false);
-                httpURLConnection.setDoOutput(true);
-                httpURLConnection.setDoInput(true);
-                httpURLConnection.setRequestMethod("POST"); // here you are telling that it is a POST request, which can be changed into "PUT", "GET", "DELETE" etc.
-                //httpURLConnection.setRequestProperty("Content-Type", "application/json"); // here you are setting the `Content-Type` for the data you are sending which is `application/json`
-                OutputStream os = httpURLConnection.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
-                HashMap<String,String> toPass = new HashMap<>();
-                if(params[0].isMailLoginData()){
-                    toPass.put("email",params[0].getEmail());
-                }else{
-                    toPass.put("username",params[0].getUsername());
-                }
-                toPass.put("password",params[0].getPassword());
-                writer.write(getPostDataString(toPass));
-                writer.flush();
-                writer.close();
-                os.close();
-                httpURLConnection.connect();
-                int responseCode = httpURLConnection.getResponseCode();
-                if(responseCode == httpURLConnection.HTTP_OK){
-                    InputStream inputStream = httpURLConnection.getInputStream();
-                    rd = new BufferedReader(new InputStreamReader(inputStream));
-                    String line = "";
-                    while ((line = rd.readLine())!= null){
-                        response.append(line);
+                    httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                    httpURLConnection.setUseCaches(false);
+                    httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setDoInput(true);
+                    httpURLConnection.setRequestMethod("POST"); // here you are telling that it is a POST request, which can be changed into "PUT", "GET", "DELETE" etc.
+                    //httpURLConnection.setRequestProperty("Content-Type", "application/json"); // here you are setting the `Content-Type` for the data you are sending which is `application/json`
+                    OutputStream os = httpURLConnection.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                    HashMap<String, String> toPass = new HashMap<>();
+                    if (params[0].isMailLoginData()) {
+                        toPass.put("email", params[0].getEmail());
+                    } else {
+                        toPass.put("username", params[0].getUsername());
                     }
-                }
-                if (response.toString().equals(RequestResult.MAIL_NOT_PRESENT.toString())) {
-                    toReturn = new Result(RequestResult.MAIL_NOT_PRESENT);
-                    Log.d("risposta","MAIL_NOT_PRESENT");
-                } else if (response.toString().equals(RequestResult.WRONG_PASSWORD.toString())) {
-                    toReturn = new Result(RequestResult.WRONG_PASSWORD);
-                    Log.d("risposta","WRONG_PASSWORD");
-                } else if(response.toString().equals(RequestResult.USERNAME_NOT_PRESENT.toString())){
-                    toReturn = new Result(RequestResult.USERNAME_NOT_PRESENT);
-                    Log.d("risposta","USERNAME_NOT_PRESENT");
+                    toPass.put("password", params[0].getPassword());
+                    writer.write(getPostDataString(toPass));
+                    writer.flush();
+                    writer.close();
+                    os.close();
+                    httpURLConnection.connect();
+                    int responseCode = httpURLConnection.getResponseCode();
+                    if (responseCode == httpURLConnection.HTTP_OK) {
+                        InputStream inputStream = httpURLConnection.getInputStream();
+                        rd = new BufferedReader(new InputStreamReader(inputStream));
+                        String line = "";
+                        while ((line = rd.readLine()) != null) {
+                            response.append(line);
+                        }
+                    } else {
+                        return new Result(RequestResult.NO_CONNECTION);
+                    }
+                    if (response.toString().equals(RequestResult.MAIL_NOT_PRESENT.toString())) {
+                        toReturn = new Result(RequestResult.MAIL_NOT_PRESENT);
+                        Log.d("risposta", "MAIL_NOT_PRESENT");
+                    } else if (response.toString().equals(RequestResult.WRONG_PASSWORD.toString())) {
+                        toReturn = new Result(RequestResult.WRONG_PASSWORD);
+                        Log.d("risposta", "WRONG_PASSWORD");
+                    } else if (response.toString().equals(RequestResult.USERNAME_NOT_PRESENT.toString())) {
+                        toReturn = new Result(RequestResult.USERNAME_NOT_PRESENT);
+                        Log.d("risposta", "USERNAME_NOT_PRESENT");
+                    } else {
+                        try {
+                            JSONObject returned = new JSONObject(response.toString());
+                            Log.d("risposta", response.toString());
+                            Account loggedAccount = new AccountImpl.Builder()
+                                    .setBorndate(returned.getString("data_nascita"))
+                                    .setEmail(returned.getString("email"))
+                                    .setId(String.valueOf(returned.getInt("id")))
+                                    .setName(returned.getString("nome"))
+                                    .setSurname(returned.getString("cognome"))
+                                    .setPassword(returned.getString("password"))
+                                    .setUsername(returned.getString("username"))
+                                    .build();
+                            toReturn = new Result(RequestResult.OK_LOGIN, loggedAccount);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
                 } else {
-                    try {
-                        JSONObject returned = new JSONObject(response.toString());
-                        Log.d("risposta",response.toString());
-                        Account loggedAccount = new AccountImpl.Builder()
-                                .setBorndate(returned.getString("data_nascita"))
-                                .setEmail(returned.getString("email"))
-                                .setId(String.valueOf(returned.getInt("id")))
-                                .setName(returned.getString("nome"))
-                                .setSurname(returned.getString("cognome"))
-                                .setPassword(returned.getString("password"))
-                                .setUsername(returned.getString("username"))
-                                .build();
-                        toReturn = new Result(RequestResult.OK_LOGIN,loggedAccount);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
+                    return new Result(RequestResult.NO_CONNECTION);
                 }
+
+
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
-                if( rd != null){
+                if (rd != null) {
                     try {
                         rd.close();
-                    }catch (Exception e){
+                    } catch (Exception e) {
 
                     }
                 }
-                if(httpURLConnection != null){
+                if (httpURLConnection != null) {
                     httpURLConnection.disconnect();
                 }
             }
