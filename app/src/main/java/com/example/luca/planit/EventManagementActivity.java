4d55c808cd86ab1e2152382e008241776c0abdbe
Luca <luca.passeri3@studio.unibo.it>
2017-07-26 12:28:57 +0200

    modified list item

--- app/src/main/java/com/example/luca/planit/EventManagementActivity.java
+++ app/src/main/java/com/example/luca/planit/EventManagementActivity.java
@@ -1,5 +1,6 @@
 package com.example.luca.planit;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
@@ -7,6 +8,7 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class EventManagementActivity extends AppCompatActivity {
 
@@ -24,11 +26,27 @@ public class EventManagementActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_management);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
         pager = (ViewPager) findViewById(R.id.pager);
         pagerAdapter = new EventManagementActivity.ScreenSlidePagerAdapter(getSupportFragmentManager());
         pager.setAdapter(pagerAdapter);
+        pager.setCurrentItem(1);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(pager, true);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 
     private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
--- app/src/main/java/com/example/luca/planit/EventOrganizedFragment.java
+++ app/src/main/java/com/example/luca/planit/EventOrganizedFragment.java
@@ -2,6 +2,7 @@ package com.example.luca.planit;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
@@ -11,6 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -58,6 +60,14 @@ public class EventOrganizedFragment extends Fragment {
 
         adapter = new EventListAdapter(getActivity(), R.layout.list_item, dataset);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(EventOrganizedFragment.this.getActivity(), EventManagementActivity.class);
+                startActivity(intent);
+            }
+        });
 
         this.startTask();
 
